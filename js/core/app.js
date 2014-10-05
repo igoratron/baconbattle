d8bfbd9f54bb183a41252c6eafadd93cba8a4fc3
Igor Czerwinski <igor@last.fm>
2014-10-05 23:24:19 +0100

    Add simple setup and start the game.

--- js/core/app.js
+++ js/core/app.js
@@ -1,7 +1,21 @@
 define(function(require) {
-   return {
-       start: function() {
-           console.log('start');
-       }
-   };
+    'use strict';
+
+    var Game = require('core/game'),
+        Board = require('core/board');
+
+    var BOARD = {
+        width: 10,
+        height: 10
+    };
+
+    return {
+        start: function() {
+            var strips = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1],
+                board = new Board(BOARD.width, BOARD.height),
+                game = new Game(board, strips);
+
+            game.start();
+        }
+    };
 });
